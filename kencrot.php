46ae0c243d3483b91f068ec04ed2d49a76fbb8c9
fikriabdillah96 <48799305+fikriabdillah96@users.noreply.github.com>
2019-03-21 23:35:49 +0700

    Update kencrot.php

--- kencrot.php
+++ kencrot.php
@@ -457,7 +457,7 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, '{
 	"name": "'.$random_nama.' '.$random_nama2.'",
 	"phone": "'.$phone_number.'",
 	"push_type": "gcm",
-	"referral_code": "FIKR54MY9",
+	"referral_code": "DENYKWOEX5",
 	"token": "c-PeIXwYYwg:APA91bHHQLHnS0FvSIOYJpN-hBJXYHxc1xQh8FrMZaQawBVPVyXxk77vTz7LWC4rtApBrZb3p4pOwJRD2JBMq0u3sChUgpasQFGcN_HNAGCscrcREwL-trFIBX3votCcFY1bn7eBmuCd",
 	"updated_at": 0
 }');
